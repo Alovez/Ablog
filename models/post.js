1c6c04a4e5aaeee7d2c99391f5ac084c590c6c32
AloveZ <Ruinand@live.com>
2016-03-15 19:02:19 +0800

    article and user page

--- models/post.js
+++ models/post.js
@@ -48,7 +48,7 @@ Post.prototype.save = function(callback) {
     });
 };
 
-Post.get = function(name, callback){
+Post.getAll = function(name, callback){
     
     mongodb.open(function(err, db){
         if(err){
@@ -79,3 +79,29 @@ Post.get = function(name, callback){
         });
     });
 };
+
+Post.getOne = function(name, day, title, callback) {
+    mongodb.open(function(err, db){
+        if(err){
+            return callback(err);
+        }
+        db.collection('post', function(err, collection){
+            if(err){
+                mongodb.close();
+                return callback(err);
+            }
+            collection.findOne({
+                "name": name,
+                "time.day": day,
+                "title": title,
+            }, function(err, doc){
+                mongodb.close();
+                if(err){
+                    return callback(err);
+                }
+                doc.post = markdown.toHTML(doc.post);
+                callback(null, doc);
+            })
+        })
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var debug = require('debug')('Ablog:server');
 module.exports = function(app) {
    
     app.get('/',function(req, res){
-        Post.get(null, function(err, posts){
+        Post.getAll(null, function(err, posts){
             if(err){
                 posts = [];
             }
@@ -144,6 +144,44 @@ module.exports = function(app) {
         req.flash('success', "文件上传成功!");
         res.redirect('/upload');
     })
+    
+    app.get('/u/:name', function(req, res){
+        User.get(req.params.name, function(err, user){
+            if(!user){
+                req.flash('error', '提供的用户不存在，请核实后再试！');
+                return res.redirect('/');
+            }
+            Post.getAll(user.name, function(err, posts){
+                if(err){
+                    req.flash('error',err);
+                    return res.redirect('/');
+                }
+                res.render('user', {
+                    title: user.name,
+                    user: req.session.user,
+                    posts: posts,
+                    success: req.flash('success').toString(),
+                    error: req.flash('error').toString()
+                });
+            });
+        });
+    });
+    
+    app.get('/u/:name/:day/:title', function(req, res){
+        Post.getOne(req.params.name, req.params.day, req.params.title, function(err, post){
+            if(err){
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('article', {
+                title: req.params.title,
+                user: req.session.user,
+                post: post,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
+    });
     
     function checkLogin(req, res, next) {
         if(!req.session.user){
