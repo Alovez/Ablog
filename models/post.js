9a6b45dced69c37c1ff0e0db408365669d87aed6
AloveZ <Ruinand@live.com>
2016-03-30 15:38:43 +0800

    comment

--- models/post.js
+++ models/post.js
@@ -23,7 +23,8 @@ Post.prototype.save = function(callback) {
         name: this.name,
         time: time,
         title: this.title,
-        post: this.post
+        post: this.post,
+        comments: []
     };
     
     mongodb.open(function(err, db){
@@ -99,7 +100,12 @@ Post.getOne = function(name, day, title, callback) {
                 if(err){
                     return callback(err);
                 }
-                doc.post = markdown.toHTML(doc.post);
+                if(doc){
+                    doc.post = markdown.toHTML(doc.post);
+                    doc.comments.fopreach(function(comment){
+                        Comment.comtent = markdown.toHTML();
+                    })
+                }
                 callback(null, doc);
             })
         })
